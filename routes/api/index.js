3f635dfda18d0adb6fe4fb2cb534a201e51e077d
Gus Miller <gustavo.miller@miller-hs.com>
2023-12-12 11:02:10 -0500

    Separate requirements

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,10 +8,8 @@
  * gustavo.miller@miller-hs.com 
  *******************************************************************/
 const router = require('express').Router();
-const videoRoutes = require('./videoRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/videos', videoRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
