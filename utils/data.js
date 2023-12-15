e8d12a1f4312211cf720a351b59912b177c23688
Gustavo Miller <gustavo.miller@miller-hs.com>
2023-12-14 21:31:45 -0500

    API Create not working - seeding thoughts as well

--- utils/data.js
+++ utils/data.js
@@ -3,7 +3,30 @@ const users = [
      { username: "Mirtha Miller", email: "mirtha@miller-hs.com" },
      { username: "Camelia Miller", email: "camelia@miller-hs.com" },
      { username: "Felipe Miller", email: "felipe@miller-hs.com" },
+     { username: "Christina Miller", email: "christina@miller-hs.com" },
+     { username: "Kevin Miller", email: "kevin@miller-hs.com" },
+     { username: "Sebastian Echegoyen", email: "sebas@miller-hs.com" },
 ]
 
+const thoughts = [
+     { thoughtText: "Act as if what you do makes a difference. It does.", username: "felipe.miller@miller-hs.com"},
+     { thoughtText: "Believe you can and you're halfway there.", username: "mirtha@miller-hs.com"},
+     { thoughtText: "Life is like riding a bicycle. To keep your balance, you must keep moving.", username: "mirtha@miller-hs.com"},
+     { thoughtText: "You are never too old to set another goal or to dream a new dream.", username: "gustavo.miller@miller-hs.com"},
+     { thoughtText: "Positive motivating quotes, captions, messages by C.S. Lewis quote", username: "mirtha@miller-hs.com"},
+     { thoughtText: "It is never too late to be what you might have been.", username: "mirtha@miller-hs.com"},
+     { thoughtText: "Positive motivating quotes, captions, messages by George Eliot quote", username: "felipe.miller@miller-hs.com"},
+     { thoughtText: "Some people look for a beautiful place. Others make a place beautiful.", username: "christina@miller-hs.com"},
+     { thoughtText: "Positive motivating quotes, captions, messages by Hazrat Inayat Khan quote", username: "camelia@miller-hs.com"},
+     { thoughtText: "We must be willing to let go of the life we planned so as to have the life that is waiting for us.", username: "felipe.miller@miller-hs.com"},
+     { thoughtText: "Happiness is not by chance, but by choice.", username: "mirtha@miller-hs.com"},
+     { thoughtText: "Positive motivating quotes, captions, messages by Jim Rohn quote", username: "camelia@miller-hs.com"},
+     { thoughtText: "If I cannot do great things, I can do small things in a great way.", username: "camelia@miller-hs.com"},
+     { thoughtText: "Positive motivating quotes, captions, messages by Martin Luther King, Jr. quote", username: "kevin@miller-hs.com"},
+     { thoughtText: "My mission in life is not merely to survive, but to thrive.", username: "mirtha@miller-hs.com"},
+     { thoughtText: "Positive motivating quotes, captions, messages by Maya Angelou quote", username: "sebas@miller-hs.com"},
+     { thoughtText: "You are enough just as you are.", username: "felipe@miller-hs.com"},
+     { thoughtText: "The bad news is time flies. The good news is you're the pilot.", username: "mirtha@miller-hs.com"}
+]
 // Export the functions for use in seed.js
-module.exports = { users };
+module.exports = { users, thoughts };
--- utils/seed.js
+++ utils/seed.js
@@ -7,14 +7,31 @@
  * Date : 12/08/2023 14:01:28 PM
  * gustavo.miller@miller-hs.com 
  *******************************************************************/
+const { json } = require('express');
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
-const { users } = require('./data');
+const Thoughts = require('../models/Thought');
+const { users, thoughts } = require('./data');
 
 connection.on('error', (err) => err);
 
+async function createUser(value) {
+
+     const user = await User.findOne()
+          .where('email').equals(value.username)
+          .exec((err, result) => {
+               if (err) {
+                    console.error(err);
+               } else {
+                    console.log(result);
+               }
+          });
+
+     await Thoughts.insertOne({ "thoughtText": value.Thought, "username": user.id });
+     return userId;
+}
+
 connection.once('open', async () => {
-     console.log('connected');
      // Delete the collections if they exist
      let thoughtsCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
      if (thoughtsCheck.length) {
@@ -27,7 +44,11 @@ connection.once('open', async () => {
      }
 
      await User.insertMany(users);
-     
+
+     // thoughts.forEach(thought => {
+     //      createUser(thought);
+     // });
+
      console.info('Seeding complete! 🌱');
      process.exit(0);
 });
